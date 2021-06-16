a7ff047fd601dfdbcc2284d4adb25f235968cc8c
ShawTim <shawtim.liu@gmail.com>
2021-06-16 10:58:44 +0800

    github path issue

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,7 +4,7 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import errorReducer from '@features/error/slice';
 import githubReducer from '@features/github/slice';
 
-export const history = createBrowserHistory();
+export const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
 
 export const rootReducer = combineReducers({
   router: connectRouter(history),
